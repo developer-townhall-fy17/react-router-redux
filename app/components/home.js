3c4ac8efe8e6b652ce00305f181cedb21ad29ae4
Sebastien Cvilic <sebastien.cvilic@accenture.com>
2017-03-02 16:49:01 +0100

    code clean-up

--- app/components/home.js
+++ app/components/home.js
@@ -1,21 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-const Home = React.createClass({
-  render: function() {
-    return (
-      <div className="home-page">
-        <h1>The app is now using Redux</h1>
-        <p>
-          While the <a href="#">CSS-Tricks article</a> for
-          this guide covers an explanation of <strong>Redux</strong>, there
-          are still many implementation details in this code that the article
-          doesn't cover. For a better understanding of those details, see
-          the <a href="https://github.com/bradwestfall/CSS-Tricks-React-Series">Github documentation</a> for
-          this guide.
-        </p>
-      </div>
-    );
-  }
-});
-
-export default Home;
+export default () => (
+  <div className="home-page">
+    <h1>Welcome</h1>
+    <p>
+      This simple HTML5 application is using <a href="https://facebook.github.io/react/">React</a>, <a href="https://github.com/ReactTraining/react-router">React Router</a> and <a href="https://github.com/reactjs/redux">Redux</a>.
+    </p>
+  </div>
+)
